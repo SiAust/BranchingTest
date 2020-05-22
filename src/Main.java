0372e6cf24b2fb7cc8d1698e3c3de85c3ca6946d
Simon Aust <simon.aust@googlemail.com>
2020-05-22 10:47:29 +0100

    FirstBranch additions

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        // initial commit
+        System.out.println("This is a change made in branch \"FirstBranch\"");
     }
 }
