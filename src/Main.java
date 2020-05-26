08b4fd0e1d325a08076377659f8916aacd2da0bf
Simon Aust <simon.aust@googlemail.com>
2020-05-26 14:15:02 +0100

    forgot Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is a change made in branch \"FirstBranch\"");
+        System.out.println("More changes have been made.");
     }
 }
